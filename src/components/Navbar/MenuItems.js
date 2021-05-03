b56b18e522cbae332663c83496633e868c015510
VicVerevita <0victor.gabriel01@gmail.com>
2021-05-03 12:07:31 +0300

    Navbar prov finish

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,29 +1,27 @@
 const MenuItems = [
+	{
+		title: "Home",
+		url: "#",
+		cName: "nav-links",
+	},
 
-    {
-        title: 'Home',
-        url: '#',
-        cName: 'nav-links'
-    },
+	{
+		title: "Top Scores",
+		url: "#",
+		cName: "nav-links",
+	},
 
-    {
-        title: 'Top Scores',
-        url: '#',
-        cName: 'nav-links'
-    },
+	{
+		title: "Contact Us",
+		url: "#",
+		cName: "nav-links",
+	},
 
-    {
-        title: 'Contact Us',
-        url: '#',
-        cName: 'nav-links'
-    },
-
-    {
-        title: 'Sign-Up',
-        url: '#',
-        cName: 'nav-links-mobile'
-    },
-
-]
+	{
+		title: "Sign-Up",
+		url: "#",
+		cName: "nav-links",
+	},
+];
 
 export default MenuItems
