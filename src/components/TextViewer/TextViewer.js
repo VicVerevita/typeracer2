1722fb2c9317967c251e4700ff2ec69e6a4b3d32
Rusu Dinu <dinu@codingshadows.com>
2021-05-03 13:29:01 +0300

    readded the comment in the TextViewer screen

--- src/components/TextViewer/TextViewer.js
+++ src/components/TextViewer/TextViewer.js
@@ -1,28 +1,29 @@
 import React, {Component} from "react";
 import "./TextViewer.css";
-import TextFetch from "../TextFetch/TextFetch";
 
 class TextViewer extends Component {
-	state = {
-		loading: true,
-		quote: null,
-	};
+    state = {
+        loading: true,
+        quote: null,
+    };
 
-	async componentDidMount() {
-		const url = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
-		const response = await fetch(url);
-		const data = await response.json();
-		this.setState({quote: data.quotes[0], loading: false});
-		console.log(data.quotes[0]);
-	}
+    async componentDidMount() {
+        const url = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({quote: data.quotes[0], loading: false});
+        console.log(data.quotes[0]);
+    }
 
-	render() {
-		return (
-			<div className="text-viewer-body">
-				<p></p>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="text-viewer-body">
+                <p>
+                    {/*TODO WRITE HERE THE TEXTFETCH HTML CODE*/}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default TextViewer;
