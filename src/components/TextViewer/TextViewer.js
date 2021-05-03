1e6324bcac5dac92ae1bb80bed1b61e224ebc1ae
Rusu Dinu <dinu@codingshadows.com>
2021-05-03 13:08:44 +0300

    merged files TextFetch and TextViewer

--- src/components/TextViewer/TextViewer.js
+++ src/components/TextViewer/TextViewer.js
@@ -3,15 +3,28 @@ import "./TextViewer.css";
 import TextFetch from "../TextFetch/TextFetch";
 
 class TextViewer extends Component {
-	render() {
-		return (
-			<div className="text-viewer-body">
-				<p>
-					<TextFetch />
-				</p>
-			</div>
-		);
-	}
+    state = {
+        loading: true,
+        quote: null,
+    };
+
+    async componentDidMount() {
+        const url = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({quote: data.quotes[0], loading: false});
+        console.log(data.quotes[0]);
+    }
+    
+    render() {
+        return (
+            <div className="text-viewer-body">
+                <p>
+                    <TextFetch/>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default TextViewer;
